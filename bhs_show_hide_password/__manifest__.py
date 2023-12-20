9b29d976080268a7ca6b619489100bc62cb02e86
dungntp <dungntp.bhsoft@gmail.com>
2023-12-20 11:35:29 +0700

    v17

---
+++ bhs_show_hide_password/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Show/Hide Password',
+  "summary":  """view password at login and signup page.""",
+  "category":  "Website",
+  "version":  "17.0",
+  'author': 'Bac Ha Software',
+  'company': 'Bac Ha Software',
+  'maintainer': 'Bac Ha Software',
+  'website': "https://bachasoftware.com",
+  "description":  "You can show and hide your password in login screen.",
+  "depends":  ['web'],
+  "data":  ['views/auth_signup_login.xml'],
+  'images': ['static/description/banner.jpg'],
+  "application":  True,
+  "installable":  True,
+  "auto_install":  False,
+  'assets': {
+      'web.assets_frontend': [
+          'bhs_show_hide_password/static/src/scss/bhs_show_hide_password.scss',
+          'bhs_show_hide_password/static/src/js/bhs_show_hide_password.js',
+      ],
+  },
+  "pre_init_hook":  "pre_init_check",
+}
---
+++ bhs_show_hide_password/static/src/js/bhs_show_hide_password.js
@@ -0,0 +1,18 @@
+/** @odoo-module **/
+
+$(document).ready(function() {
+    $('.oe_website_login_container').each(function(ev) {
+        var oe_website_login_container = this;
+
+        $(oe_website_login_container).on('click', 'div.input-group-append .show_hide_pass', function() {
+            var icon = $(this).find('i.fa.fa-eye').length
+            if (icon == 1) {
+                $(this).find('i.fa.fa-eye').removeClass('fa-eye').addClass('fa-eye-slash');
+                $(this).parent().prev('input[type="password"]').prop('type', 'text');
+            } else {
+                $(this).find('i.fa.fa-eye-slash').removeClass('fa-eye-slash').addClass('fa-eye');
+                $(this).parent().prev('input[type="text"]').prop('type', 'password');
+            }
+        });
+    });
+});
